7e6afd6d27475861feaa97e73c29e588e73c736f
Ssparker <47154706+Ssparker@users.noreply.github.com>
2019-02-17 21:23:38 -0600

    testtesttest

---
+++ src/SundayTest/SundayTest.java
@@ -0,0 +1,11 @@
+package SundayTest;
+
+import java.sql.SQLOutput;
+
+public class SundayTest {
+
+    public static void main(String[] args) {
+
+        System.out.println("Happy Sunday");
+    }
+}
